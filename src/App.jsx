04745dd79f7d72b5244d5747a3a0ab91c232df99
pisitpongtae-code <pisitpong.tae@gmail.com>
2025-09-01 20:25:33 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,11 @@
+import React from "react";
+import Website from "./Website";
+
+export default function App() {
+  return (
+    <div className="min-h-screen">
+      <Website />
+    </div>
+  );
+}
 
